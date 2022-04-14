47c1dd43600553e6f19bf68cbfef0dce54c1ee42
Ashlyn Lanaville <a14079@MacBook-Air.local>
2022-04-14 13:54:50 -0700

    added properties to model

--- recipes_api/models.py
+++ recipes_api/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Recipes(models.Model):
     name = models.CharField(max_length=32)
     link = models.URLField(max_length=200)
     image = models.ImageField(upload_to='recipe_images', default='default-image.png')
-    # directions = ArrayField(models.CharField(max_length=50))
-    # ingredients = ArrayField(models.CharField(max_length=50))
+    description = models.CharField(max_length=350, null=True, blank=True)
+    directions = models.CharField(max_length=750, null=True, blank=True)
+    ingredients = models.CharField(max_length=250, null=True, blank=True)
+    tags = models.CharField(max_length=20, null=True, blank=True)
